e9a636754ac498830665d8f064840ed033fd7a73
Jordan Robitaille <jdrobitaille@email.neit.edu>
2017-01-10 08:38:37 -0500

    Basic Table Header

---
+++ mult.php
@@ -0,0 +1,12 @@
+<table border = "1">
+	<thead>
+		<tr>
+			<th>&nbsp;</th>
+				<?php
+				for ($col = 1; $col <= 19; $col++){
+					echo "<th>$col</th>";
+				}
+				?>
+		</tr>
+	</thead>
+</table>
