a7ded729931a643d008717fe8471808d25397911
Jordan Robitaille <jdrobitaille@email.neit.edu>
2017-01-10 08:50:08 -0500

    Basic Row Loop

--- mult.php
+++ mult.php
@@ -6,6 +6,11 @@
 				for ($col = 1; $col <= 19; $col++){
 					echo "<th>$col</th>";
 				}
+				for ($row = 1; $row <= 19; $row++){
+					echo "<tr>";
+						echo "<th> $row </th>";
+					echo "</tr>\n";
+				}
 				?>
 		</tr>
 	</thead>
